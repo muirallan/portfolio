7dba03d764077ab55f3e066d72dee4125d4465d9
Allan Muir <47343066+muirallan@users.noreply.github.com>
2024-10-31 13:54:01 -0400

    NEW UPDATES

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+  }
+  
+  function downloadCV() {
+    window.open('./resummeeeeee/assets/muir,Allan2024.pdf', '_blank');
+  }
+  
+  // Smooth scroll for navigation links
+  document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', event => {
+      event.preventDefault();
+      document.getElementById(link.getAttribute('href').substring(1)).scrollIntoView({ behavior: 'smooth' });
+      // Close the menu on link click for mobile
+      document.querySelector('.nav-links').classList.remove('active');
+    });
+  });
+  
